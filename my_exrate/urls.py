744f033dc0c83a1ed7571612fc274b9c1afa9cb4
vitalyby <vitaly.by@gmail.com>
2018-12-20 14:57:30 +0300

    commit number 2

--- my_exrate/urls.py
+++ my_exrate/urls.py
@@ -1,9 +1,11 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 
 from . import views
 
+app_name = 'my_exrate'
 urlpatterns = [
-    # url(r'^my_exrate/', include('my_exrate.urls')),
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
-
--- my_exrate/views.py
+++ my_exrate/views.py
@@ -1,5 +1,29 @@
 from django.http import HttpResponse
+from .models import Question
+from django.shortcuts import get_object_or_404, render
 
 
+# def index(request):
+#     return HttpResponse("Hello, world.")
 def index(request):
-    return HttpResponse("Hello, world.")
+    latest_question_list = Question.objects.order_by('id')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'my_exrate/index.html', context)
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'my_exrate/detail.html', {'question': question})
