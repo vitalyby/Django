b80b020d236a2aa52e56957a71fe116cc3a61d91
vitalyby <vitaly.by@gmail.com>
2018-12-21 22:07:22 +0300

    commit 2

--- my_exrate/views.py
+++ my_exrate/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 from .models import Question
 from django.shortcuts import get_object_or_404, render
+import requests
+import pandas as pd
 
 
 # def index(request):
@@ -22,6 +24,14 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+def rate(request, question_id):
+    url = 'http://www.nbrb.by/API/ExRates/Rates?Periodicity=0'
+    # tables = pd.read_html(requests.get(url).text, header=0)
+    a = requests.get(url).text
+    b = pd.Series(a)
+
+    return HttpResponse("выводим массив \n %s." % b)
 
 
 def detail(request, question_id):
